e8f89ff1dca97bbfcd525597586ea76091efeff9
yeonjoo.lee <teddyjune@naver.com>
2022-09-01 22:01:36 +0900

    feat: TabBar 사용

--- lib/ui/movie_main_screen.dart
+++ lib/ui/movie_main_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'movie_search_screen.dart';
 import 'movie_view_model.dart';
 
 class MovieMainScreen extends StatefulWidget {
-
   const MovieMainScreen({Key? key}) : super(key: key);
 
   @override
@@ -18,37 +16,39 @@ class _MovieMainScreenState extends State<MovieMainScreen> {
     final viewModel = context.watch<MovieViewModel>();
     final state = viewModel.state;
 
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          elevation: 0,
-          centerTitle: true,
-          leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-          title: const Text(
-            'NETFLIX',
-            style: TextStyle(color: Colors.redAccent),
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            elevation: 0,
+            centerTitle: true,
+            title: const Text(
+              'NETFLIX',
+              style: TextStyle(color: Colors.redAccent),
+            ),
+            bottom: const TabBar(
+              tabs: [
+                Text('TV 프로그램'),
+                Text('영화'),
+                Text('내가 찜한 콘텐츠'),
+              ],
+            ),
           ),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
-            IconButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const MovieSearchScreen(),
-                      ));
-                },
-                icon: const Icon(Icons.search)),
-          ],
-        ),
-        body: SizedBox(
-          width: double.infinity,
-          height: 400,
-          child: Image.network(
-            'https://image.tmdb.org/t/p/w500/nmGWzTLMXy9x7mKd8NKPLmHtWGa.jpg',
-            fit: BoxFit.cover,
-          ),
-        ),
-    );
+          body: const TabBarView(
+            children: [
+              Image(image: AssetImage('assets/liberation.png')),
+              Image(image: AssetImage('assets/645.png')),
+              Image(image: AssetImage('assets/liberation.png')),
+            ]),
+        ));
   }
 }
+// SizedBox(
+// width: double.infinity,
+// height: 400,
+// child: Image.network(
+// 'https://image.tmdb.org/t/p/w500/nmGWzTLMXy9x7mKd8NKPLmHtWGa.jpg',
+// fit: BoxFit.cover,
+// ),
+// ),
