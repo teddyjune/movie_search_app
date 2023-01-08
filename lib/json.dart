6637ae5975b899f671045b441a6df27dfc3cefe9
yeonjoo.lee <teddyjune@naver.com>
2023-01-08 21:56:32 +0900

    feat: 최신등록콘텐츠 섹션 구현

---
+++ lib/json.dart
@@ -0,0 +1,7 @@
+const List myList = [
+  {"img": "assets/latest_1.jpg"},
+  {"img": "assets/latest_2.jpeg"},
+  {"img": "assets/latest_3.jpg"},
+  {"img": "assets/latest_4.jpg"},
+  {"img": "assets/latest_5.jpg"},
+];
